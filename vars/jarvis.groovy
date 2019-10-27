85ddb51fcbab3e9371d88ceae50ece6def4dbc6d
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 04:34:53 -0700

    debug

--- vars/jarvis.groovy
+++ vars/jarvis.groovy
@@ -6,7 +6,7 @@ def call(Closure body) {
         }
         def context(context) {
             context.metaClass.methodMissing { name, args ->
-                return context.jarvisHcl.call1(name, args)
+                return context.jarvisHcl.get(name, args)
             }
         }
     }
