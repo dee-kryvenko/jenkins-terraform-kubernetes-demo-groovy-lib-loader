fd31a09f24b9aadf3b613847d24cab7b3811e43b
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 02:33:43 -0700

    add methodMissing

--- vars/jarvis.groovy
+++ vars/jarvis.groovy
@@ -10,3 +10,12 @@ def call(Closure body) {
     body.call()
     library "jarvis@${jarvis.version}"
 }
+
+def methodMissing(String name, args) {
+
+  if (args.length == 3 && args[0] instanceof String && args[1] instanceof String && args[2] instanceof Closure) {
+    println "${name}.${args[0]}.${args[1]}"
+  }
+
+  throw new MissingMethodException(name, this.class, args)
+}
--- vars/methodMissing.groovy
+++
@@ -1,8 +0,0 @@
-def methodMissing(String name, args) {
-
-  if (args.length == 3 && args[0] instanceof String && args[1] instanceof String && args[2] instanceof Closure) {
-    println "${name}.${args[0]}.${args[1]}"
-  }
-
-  throw new MissingMethodException(name, this.class, args)
-}
