03440819361e58aea7f66486308526ff48d7e515
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 02:58:54 -0700

    debut

--- vars/jarvis.groovy
+++ vars/jarvis.groovy
@@ -1,5 +1,3 @@
-echo this
-
 def call(Closure body) {
     def jarvis = new Object() {
         def version
@@ -11,13 +9,4 @@ def call(Closure body) {
     body.setResolveStrategy(Closure.DELEGATE_FIRST)
     body.call()
     library "jarvis@${jarvis.version}"
-}
-
-def methodMissing(String name, args) {
-
-  if (args.length == 3 && args[0] instanceof String && args[1] instanceof String && args[2] instanceof Closure) {
-    println "${name}.${args[0]}.${args[1]}"
-  }
-
-  throw new MissingMethodException(name, this.class, args)
 }
