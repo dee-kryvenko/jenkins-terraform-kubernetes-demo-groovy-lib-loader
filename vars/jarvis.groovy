4ec0c6caa739abd8c2e31f2a460fc77797a02c3d
Dee Kryvenko <dee.kryvenko@sony.com>
2019-10-27 17:45:56 -0700

    debug

--- vars/jarvis.groovy
+++ vars/jarvis.groovy
@@ -2,7 +2,8 @@ def call(Closure body) {
     def jarvis = new Object() {
         def version
         void jarvis(context) {
-            context.metaClass.originalMethodMissing = context.metaClass.methodMissing
+            // context.metaClass.originalMethodMissing = context.metaClass.methodMissing
+            context.metaClass.originalMethodMissing = context.metaClass.getMetaMethod("methodMissing", [String, []] as Class[])
             context.metaClass.methodMissing { String name, args ->
                 return context.jarvisHcl.get(context, name, args)
             }
